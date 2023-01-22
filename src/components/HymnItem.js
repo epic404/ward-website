45ab532ab95c58eb800a59089617ba671156ca6b
sikkdev <brandonk3nt@gmail.com>
2023-01-22 01:33:00 -0700

    style updates for musical numbers, footer update, config updates for this sunday

--- src/components/HymnItem.js
+++ src/components/HymnItem.js
@@ -1,3 +1,16 @@
+/**
+ * Data for hymn items:
+ * 
+ * {
+ *   type{string}: This is the type of hymn it is- Opening Hymn, Musical Number, etc.
+ *   number{number}: The hymn number as found in the hymn book.
+ *   musicians[{string}]: Array of names of performers.
+ *   link{string}: Url to the hymn sheet music.
+ *   title{string}: The name of the song/hymn.
+ *   truncate{boolean}: If the song title should be truncated w/ ellipsis. Leave it alone unless you have a good reason not to.
+ * }
+ */
+
 export default function HymnItem({ hymn }) {
   hymn.truncate = hymn.truncate ?? true; // Default's to true.
 
@@ -5,8 +18,13 @@ export default function HymnItem({ hymn }) {
     <div className="py-2">
       <div className="flex">
         <div className="flex-none px-1">{hymn.type}</div>
-        <div className="flex-1 mb-1 border-b border-dashed border-stone-300"></div>
-        <div className="flex-none px-1">{hymn.number}</div>
+        <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18"></div>
+        {hymn.number && <div className="flex-none px-1">{hymn.number}</div>}
+        {hymn.musicians && (
+          <div className="flex-none px-1 text-right">
+            {hymn.musicians.map((musician) => <div>{musician}</div>)}
+          </div>
+        )}
       </div>
       <div className={`text-center font-sm ${hymn.truncate ? 'truncate zero-auto max-75' : ''}`}>
         {hymn.link
--- src/components/SpeakerItem.js
+++ src/components/SpeakerItem.js
@@ -2,7 +2,7 @@ export default function SpeakerItem({ speaker }) {
   return (
     <div className="flex py-2">
       <div className="flex-none px-1">{speaker.type}</div>
-      <div className="flex-1 mb-1 border-b border-dashed border-stone-300"></div>
+      <div className="flex-1 mb-1 border-b border-dashed border-stone-300 height-18"></div>
       <div className="flex-none px-1">{speaker.name}</div>
     </div>
   );
