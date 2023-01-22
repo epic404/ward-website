f782093afae4a2f9d9bcf6cf50f35dc82367bdcc
sikkdev <brandonk3nt@gmail.com>
2023-01-22 08:02:25 -0700

    ward biz

---
+++ src/components/BusinessItem.js
@@ -0,0 +1,10 @@
+// export default function BusinessItem({item}) {
+//   return ((item.length > 0) &&
+//       <div>
+//         <h2 className="mb-2 text-lg font-bold text-gray-900">Baptisms</h2>
+//         <ul>
+//           {business.baptisms.map((item) => <li key={v4()}>{item}</li>)}
+//         </ul>
+//       </div>
+//   );
+// }
