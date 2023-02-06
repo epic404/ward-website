2b0bef2ac862a38613f4e24846d2176e737eff90
sikkdev <brandonk3nt@gmail.com>
2023-02-06 12:48:50 -0700

    new favicon and imgaes

--- src/components/TitleBlock.js
+++ src/components/TitleBlock.js
@@ -9,7 +9,7 @@ export default function TitleBlock({ ward }) {
   }, []);
 
   return [
-    <img key="title-logo" src="./modern-logo.png" className="zero-auto w-3/4 md:w-1/2" alt="church-logo" />,
+    <img key="title-logo" src="/images/modern-logo.png" className="zero-auto w-3/4 md:w-1/2" alt="church-logo" />,
     <div key="title-credits" className="church-font text-center p-1 py-2">
       <p className="font-md">{ward}</p>
       <p className="font-lg">Sacrament Meeting</p>
