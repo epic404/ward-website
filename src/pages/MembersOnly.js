c4ca6d84f269bcac0e625d8a348d8439cfec705a
sikkdev <brandonk3nt@gmail.com>
2023-02-19 00:04:24 -0700

    updated 403

--- src/pages/MembersOnly.js
+++ src/pages/MembersOnly.js
@@ -1,5 +1,23 @@
 export default function MembersOnly() {
+  const styles = {
+    image: {
+      height: '50vh',
+      margin: '0 auto',
+      padding: 16
+    },
+    text: {
+      textAlign: 'center',
+      fontWeight: '800',
+      padding: 8
+    }
+  };
+
   return (
-    <div>Order of Services is visible via distributed link only.</div>
+    <div>
+      <img style={styles.image} src="/images/church-logo.png" />
+      <div style={styles.text}>
+        <h2>The Dana Ranch Ward order of services is distributed weekly to ward members and is accessible via email link only. Please check your email inbox or contact a member of the Bishopric for assistance.</h2>
+      </div>
+    </div>
   );
 }
--- src/pages/NoConfig.js
+++ src/pages/NoConfig.js
@@ -1,5 +1,22 @@
 export default function NoConfig() {
+  const styles = {
+    image: {
+      height: '50vh',
+      margin: '0 auto',
+      padding: 16
+    },
+    text: {
+      textAlign: 'center',
+      fontWeight: '800',
+      padding: 8
+    }
+  };
+
   return (
-    <div>Please check back later this week!</div>
-  );
+    <div>
+      <img style={styles.image} src="/images/church-logo.png" />
+      <div style={styles.text}>
+        <h2>The Dana Ranch Ward order of services is not currently available, please check back in a day or two!</h2>
+      </div>
+    </div>  );
 }
