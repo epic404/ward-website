a9a2af30467d5713f7981306f0ac2264380f2c95
sikkdev <brandonk3nt@gmail.com>
2023-02-26 01:13:32 -0700

    business polish

--- src/components/BusinessItem.js
+++ src/components/BusinessItem.js
@@ -1,10 +1,21 @@
-// export default function BusinessItem({item}) {
-//   return ((item.length > 0) &&
-//       <div>
-//         <h2 className="mb-2 text-lg font-bold text-gray-900">Baptisms</h2>
-//         <ul>
-//           {business.baptisms.map((item) => <li key={v4()}>{item}</li>)}
-//         </ul>
-//       </div>
-//   );
-// }
+import { v4 } from 'uuid'
+
+export default function BusinessItem({ config, items }) {
+  const lastItem = items.length - 1;
+  return (
+    <div>
+      <hr className="h-px my-4 bg-gray-400 border-0" />
+      <h2 className="mb-2 text-lg font-bold text-gray-900 text-center">{config.label}</h2>
+      {config.prompt && <h3 className="border-l-4 border-slate-300 rounded-r-md bg-slate-200 p-2 m-2">{config.prompt}</h3>}
+      {items.map((item, index) => {
+        return (
+          <div className="text-center" key={v4()}>
+            {item}
+            {index !== lastItem && <div className="text-center">✵ ✵ ✵</div>}
+          </div>
+        );
+      })}
+      {config.promptEnd && <h3 className="border-l-4 border-slate-300 rounded-r-md bg-slate-200 p-2 m-2">{config.promptEnd}</h3>}
+    </div>
+  );
+}
--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,68 +1,14 @@
 import { v4 } from 'uuid';
+import BusinessItem from '../components/BusinessItem';
+import businessConfig from '../config/_business.json';
 
 export default function Business({ business }) {
   return (
     <div>
-      <h1 className="text-center py-2 church-font font-lg">Ward Business</h1>
-      <div className="italic text-center uppercase">**discretionary information**</div>
-      <hr className="h-px my-4 bg-gray-400 border-0" />
-
-      {(business.baptisms?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Baptisms</h2>
-          <ul>
-            {business.baptisms.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.ordinations?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Ordinations</h2>
-          <ul>
-            {business.ordinations.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.moveins?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Move-ins</h2>
-          <ul>
-            {business.moveins.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.other?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">**Other</h2>
-          <ul>
-            {business.other.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.releases?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Releases</h2>
-          <ul>
-            {business.releases.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.callings?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Callings</h2>
-          <ul>
-            {business.callings.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
-      {(business.deaths?.length > 0) && (
-        <div>
-          <h2 className="mb-2 text-lg font-bold text-gray-900">Deaths</h2>
-          <ul>
-            {business.deaths.map((item) => <li key={v4()}>{item}</li>)}
-          </ul>
-        </div>
-      )}
+      <h1 className="text-center py-2 church-font font-lg">Announcements</h1>
+      {
+        businessConfig.business.map((config) => (business[config.type].length > 0) && (<BusinessItem key={v4()} config={config} items={business[config.type]} />))
+      }
     </div>
   );
 }
